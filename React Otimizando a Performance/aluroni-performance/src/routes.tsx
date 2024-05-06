ee3f045e2612e65a7d8b656a474f1ca47d469ea4
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2024-05-06 18:13:37 -0300

    curso React: otimizando a performance

---
+++ React Otimizando a Performance/aluroni-performance/src/routes.tsx
@@ -0,0 +1,34 @@
+import Footer from 'components/Footer';
+import Menu from 'components/Menu';
+import { Suspense, lazy } from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+export default function AppRouter() {
+
+  const Cardapio = lazy(() => import('pages/Cardapio'));
+  const PaginaPadrao = lazy(() => import('components/PaginaPadrao'));
+  const Inicio = lazy(() => import('pages/Inicio'));
+  const NotFound = lazy(() => import('pages/NotFound'));
+  const Prato = lazy(() => import('pages/Prato'));
+  const Sobre = lazy(() => import('pages/Sobre'));
+
+  return (
+    <main className='container'>
+      <Router>
+        <Menu />
+        <Suspense fallback={<p> Carregando... </p>}>
+          <Routes>
+            <Route path='/' element={<PaginaPadrao />}>
+              <Route index element={<Inicio />} />
+              <Route path='cardapio' element={<Cardapio />} />
+              <Route path='sobre' element={<Sobre />} />
+            </Route>
+            <Route path='prato/:id' element={<Prato />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </Suspense>
+        <Footer />
+      </Router>
+    </main>
+  );
+}
