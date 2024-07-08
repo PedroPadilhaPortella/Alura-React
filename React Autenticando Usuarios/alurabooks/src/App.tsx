a7328860684ebc2708099129591b4e898b90962e
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2024-07-08 15:35:09 -0300

    curso React: autenticando usuários

---
+++ React Autenticando Usuarios/alurabooks/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
+import './App.css';
+import Rotas from './rotas';
+import { createBrowserHistory } from 'history'
+
+export const history = createBrowserHistory({ window })
+
+function App() {
+  return (
+    <HistoryRouter history={history}>
+      <Rotas />
+    </HistoryRouter>
+  );
+}
+
+export default App;
